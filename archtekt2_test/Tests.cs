3a6c2554c36bab5a07d206695e8731613b8047c3
stokato <demyanovpaul@yandex.ru>
2022-03-08 01:50:54 +0300

    first proj

---
+++ archtekt2_test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using archtekt2;
+using NUnit.Framework;
+
+namespace archtekt2_test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.AreEqual(new double [0], Quadratic.solve(1,0, 1));
+        }
+    }
+}
